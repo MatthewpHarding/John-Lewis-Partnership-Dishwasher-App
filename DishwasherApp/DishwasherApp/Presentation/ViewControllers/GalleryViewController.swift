f1774022f7c13d15594bfbc12543dc0cd72ce60d
Matt Harding <matthew@vitl.com>
2017-01-24 22:28:54 +0000

    The gallery is now displaying the product detail images

--- DishwasherApp/DishwasherApp/Presentation/ViewControllers/GalleryViewController.swift
+++ DishwasherApp/DishwasherApp/Presentation/ViewControllers/GalleryViewController.swift
@@ -9,27 +9,96 @@
 import UIKit
 import Kingfisher
 
+class ImageViewController: UIViewController {
+    
+    var imageURL: URL? = nil { didSet {
+        guard oldValue != imageURL else {
+            return
+        }
+        
+        imageView.kf.cancelDownloadTask()
+        imageView.kf.setImage(with: imageURL, options: [.transition(.fade(0.1))])
+        }
+    }
+    
+    let imageView = UIImageView()
+    
+    override func loadView() {
+        super.loadView()
+        imageView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        imageView.translatesAutoresizingMaskIntoConstraints = true
+        imageView.frame.size = view.bounds.size
+        view.addSubview(imageView)
+    }
+}
+
 class GalleryViewController: UIPageViewController {
     
     var imageURLs: [URL]? { didSet {
-        
+
+        reloadViewControllers()
         } }
     
- 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dataSource = self
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    private func reloadViewControllers() {
+        
+        guard let url = imageURLs?.first else {
+            setViewControllers(nil, direction: .forward, animated: false, completion: nil)
+            return
+        }
+        
+        let imageViewController = ImageViewController()
+        imageViewController.imageURL = url
+        setViewControllers([imageViewController], direction: .forward, animated: false, completion: nil)
     }
-    */
+}
 
+// MARK:- UIPageViewControllerDataSource
+
+extension GalleryViewController: UIPageViewControllerDataSource {
+    
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        return nextViewController(from: viewController, withIndexModifier: -1)
+    }
+    
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        return nextViewController(from: viewController, withIndexModifier: +1)
+    }
+    
+    private func nextViewController( from viewController: UIViewController, withIndexModifier indexModifier: Int) -> UIViewController? {
+        guard
+            let imageURLs = self.imageURLs,
+            let previousViewController = viewController as? ImageViewController,
+            let imageURL = previousViewController.imageURL,
+            let index = imageURLs.index(where: { $0 == imageURL }),
+            let nextImageURL = imageURLs[safe: index + indexModifier]
+            else {
+                return nil
+        }
+        
+        let imageViewController = ImageViewController()
+        imageViewController.imageURL = nextImageURL
+        return imageViewController
+    }
 }
